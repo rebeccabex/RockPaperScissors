f55ad1422afd9ed2869fd0d0d710208a5db0cadb
rebeccabex <rebeccabexb@gmail.com>
2018-01-25 13:32:12 +0000

    User input with random AI

--- src/main/scala/RockPaperScissors.scala
+++ src/main/scala/RockPaperScissors.scala
@@ -1,23 +1,49 @@
 object RockPaperScissors {
 
   def main(args: Array[String]): Unit = {
-    println(determineWinner("rock", "scissors"))
+    var playing = true
+    while (playing) {
+      val input = readInput("Enter choice, or 'quit' to exit: ")
+      if (input == "quit")
+        playing = false
+      else {
+        determineWinner(input, aiGuess()) match {
+          case 0 => println("Draw")
+          case 1 => println("You won!")
+          case 2 => println("You lost")
+          case _ => println("Invalid entry. Try again")
+        }
+      }
+    }
   }
-  case class RPS(player1: String, player2: String)
-  def determineWinner(p1Choice: String, p2Choice: String): Any = (rpsValue(p1Choice), rpsValue(p2Choice)) match {
+
+  def aiGuess(): String = {
+    val rand = scala.util.Random
+    rand.nextInt(3) match {
+      case 0 => "rock"
+      case 1 => "paper"
+      case 2 => "scissors"
+    }
+  }
+
+  def determineWinner(p1Choice: String, p2Choice: String): Int = (rpsValue(p1Choice), rpsValue(p2Choice)) match {
     case (a, _) if a == -1 => -1
-    case (_, b) if b == -1 => -2
+    case (_, b) if b == -1 => -1
     case (a, b) if a == b => 0
     case (a, b) => ((a - b) + 3) % 3
-    case _ => println(s"p1: $p1Choice; p2: $p2Choice. " + rpsValue(p1Choice) + "-" + rpsValue(p2Choice))
+    case _ => -1
   }
 
+  def rpsValue(choice: String): Int = choice.toLowerCase match {
+    case "rock" => 1
+    case "paper" => 2
+    case "scissors" => 3
+    case _ => -1
+  }
 
-def rpsValue(choice: String): Int = choice.toLowerCase match {
-  case "rock" => 1
-  case "paper" => 2
-  case "scissors" => 3
-  case _ => -1
-}
+  def readInput(prompt: String): String = {
+    print(prompt)
+    scala.io.StdIn.readLine()
+  }
 
 }
