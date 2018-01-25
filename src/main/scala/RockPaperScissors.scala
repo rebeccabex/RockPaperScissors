cc76d5c59b8e2296bbe84714f94cffa89d6ef5e7
rebeccabex <rebeccabexb@gmail.com>
2018-01-25 15:10:39 +0000

    Slightly improved AI

--- src/main/scala/RockPaperScissors.scala
+++ src/main/scala/RockPaperScissors.scala
@@ -1,29 +1,51 @@
 object RockPaperScissors {
 
   def main(args: Array[String]): Unit = {
+
     var playing = true
+    var humanChoice = ""
+    var aiChoice = ""
+    var winner = 0
+
+    var humanWins = 0
+    var aiWins = 0
+
     while (playing) {
-      val input = readInput("Enter choice, or 'quit' to exit: ")
-      if (input == "quit")
+      aiChoice = aiChoose(humanChoice, aiChoice, winner)
+      humanChoice = readInput("Enter choice, or 'quit' to exit: ")
+      if (humanChoice == "quit")
         playing = false
       else {
-        determineWinner(input, aiGuess()) match {
+        print("AI chose " + aiChoice + ". ")
+        winner = determineWinner(humanChoice, aiChoice)
+        winner match {
           case 0 => println("Draw")
-          case 1 => println("You won!")
-          case 2 => println("You lost")
+          case 1 => println("You won!"); humanWins += 1
+          case 2 => println("You lost!"); aiWins += 1
           case _ => println("Invalid entry. Try again")
         }
+        println("Score: " + humanWins + "-" + aiWins)
       }
     }
   }
 
-  def aiGuess(): String = {
-    val rand = scala.util.Random
-    rand.nextInt(3) match {
-      case 0 => "rock"
-      case 1 => "paper"
-      case 2 => "scissors"
+  def aiChoose(lastHumanChoice: String, lastAiChoice: String, lastWinner: Int): String = {
+    lastWinner match {
+      case 0 => aiChooseWithBias()
+      case 1 => aiChooseWithBias(losesTo(lastHumanChoice), 3)
+      case 2 => aiChooseWithBias(beats(lastAiChoice), 3)
+      case _ => aiChooseWithBias()
     }
+  }
+
+  def aiChooseWithBias(biasTowards: String = "", biasVal: Int = 0): String = {
+    val rand = scala.util.Random
+    println("Bias: " + biasTowards)
+    val randInt = rand.nextInt(3 + biasVal)
+    if (biasTowards.isEmpty || randInt < 3)
+      rpsChoiceFromVal(randInt)
+    else
+      biasTowards
   }
 
   def determineWinner(p1Choice: String, p2Choice: String): Int = (rpsValue(p1Choice), rpsValue(p2Choice)) match {
@@ -34,6 +56,11 @@ object RockPaperScissors {
     case _ => -1
   }
 
+  def readInput(prompt: String): String = {
+    print(prompt)
+    scala.io.StdIn.readLine()
+  }
+
   def rpsValue(choice: String): Int = choice.toLowerCase match {
     case "rock" => 1
     case "paper" => 2
@@ -41,9 +68,19 @@ object RockPaperScissors {
     case _ => -1
   }
 
-  def readInput(prompt: String): String = {
-    print(prompt)
-    scala.io.StdIn.readLine()
+  def rpsChoiceFromVal(choiceValue: Int): String = choiceValue match {
+    case 1 => "rock"
+    case 2 => "paper"
+    case 0 | 3 => "scissors"
+    case _ => "error"
+  }
+
+  def beats(choice: String): String = {
+    rpsChoiceFromVal((rpsValue(choice) + 2) % 3)
+  }
+
+  def losesTo(choice: String): String = {
+    rpsChoiceFromVal((rpsValue(choice) + 1) % 3)
   }
 
 }
